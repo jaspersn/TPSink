db437a84ad17472dcd1bf4ca2c79ce2279b4cbd3
Xavier Hines <xmhines01@gmail.com>
2022-05-09 18:24:14 -0700

    Changes by jasper

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -5,9 +5,29 @@ import java.awt.event.ActionListener;
 
 public class MainGUI extends JFrame implements ActionListener  {
 
-    private JPanel panel = new JPanel();
+    /**
+     * Width of GUI window, based on {@link Main#PATH_TO_LOGO} dimensions.
+     */
+    private static final int WIDTH = 684;
+    /**
+     * Height of GUI window, based on {@link Main#PATH_TO_LOGO} dimensions.
+     */
+    private static final int HEIGHT = 729;
+
+    private JPanel panel = new JPanel(new BorderLayout()) {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(new ImageIcon(Main.PATH_TO_LOGO).getImage(), 0, 0, null);
+        }
+    };
 
     public MainGUI() {
+        // Init buttonPanel that holds Main buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.setBackground(new Color(0,0,0,0));
+
         JButton button = new JButton(About.WINDOW_NAME);
         JButton LoginButton = new JButton(UserLogin.getWindowName());
         //action listener lambda implementation
@@ -23,7 +43,9 @@ public class MainGUI extends JFrame implements ActionListener  {
         setTitle(About.GROUP_NAME);
         add(panel);
         pack();
+        setSize(WIDTH, HEIGHT);
         setVisible(true);
+        setResizable(false);
     }
 
     @Override
