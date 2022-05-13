4cac7f9af41700b9d97d81d145b216739680f1f3
Xavier Hines <xmhines01@gmail.com>
2022-05-12 21:02:29 -0700

    Series of JavaDoc comments and added authors to classes. Cleaned up by taking out some comments removing a few println's and extra spaces.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-import java.io.File;
-
+/**
+ * @author Xavier Hines
+ * Date 5/12/22
+ */
 public class Main {
 
-    protected static final String PATH_TO_LOGO = "Assets/TPSink.png";
+    /**
+     * Path for main window home Image
+     */
+    private static final String PATH_TO_LOGO = "Assets/TPSink.png";
 
+    /**
+     * main method, runs program
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello new project");
-
-       // new AboutGUI();
         new MainGUI();
     }
-    
-          
-    //hello, fork demosss
+
+    /**
+     * returns main image path
+     * @return main image path
+     */
+    public static String getPathToLogo() {
+        return PATH_TO_LOGO;
+    }
 }
 
--- src/UserLogin.java
+++ src/UserLogin.java
@@ -1,38 +1,74 @@
 import javax.swing.*;
 
+/**
+ * @author Xavier Hines
+ * Date 5/9/22
+ */
 public class UserLogin {
+    /**
+     * name of the window
+     */
     private static final String WINDOW_NAME = "Login";
+    /**
+     * Where the username will be stored?
+     */
     private static String USER_NAME = "";
+    /**
+     * where the user password will be stored?
+     */
     private static String USER_PASSWORD = "";
 
+    /**
+     * @return user password
+     */
     public static String getUserPassword() {
         return USER_PASSWORD;
     }
 
     //TODO User name and password Constructor
+
+    /**
+     * @param userPassword password to set
+     */
     public static void setUserPassword(String userPassword) {
         USER_PASSWORD = userPassword;
     }
 
+    /**
+     * @return username
+     */
     public static String getUserName() {
         return USER_NAME;
     }
 
+    /**
+     * @param userName sets username
+     */
     public static void setUserName(String userName) {
         USER_NAME = userName;
     }
 
+    /**
+     * @return window name
+     */
     public static String getWindowName() {
         return WINDOW_NAME;
     }
 
-    public static void yourMethod(JLabel success, String name, String password) {
-        //test println
-        System.out.println(getUserName() + ", " + getUserPassword());
+    /**
+     * Action that will accept a string name and password when the user
+     * attempts to login and checks them against the saved username and password.
+     * @param success Label to indicate if correct or not
+     * @param name String representing username
+     * @param password String representing password
+     */
+    public static void checkUserAndPassword(JLabel success, String name, String password) {
         // TODO: Make not hard-coded
         if (name.equals(USER_NAME) &&
                 password.equals(USER_PASSWORD)) {
             success.setText("Login successful");
+        } else {
+            success.setText("Wrong User Name Or Pass");
         }
     }
 }
