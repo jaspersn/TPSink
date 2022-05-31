dbfbacc422110cbfc9a77ff16fa69eb95be46542
ltee1232 <phuocle2@uw.eedu>
2022-05-31 11:49:59 -0700

    4nd Phuoc added test classes with comments

--- src/test/java/AboutGUITest.java
+++ src/test/java/AboutGUITest.java
@@ -3,13 +3,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ *  @author Phuoc Le
+ *  Date 5/31/22
+ *
+ * This class is made to test out UserGUI class.
+ */
 class AboutGUITest {
     private AboutGUI aboutG;
+
+    /**
+     * Set up before we test all methods
+     */
     @BeforeEach
     void setUp() {
         aboutG = new AboutGUI();
     }
 
+    /**
+     * testNonNull is to check if the initiation of object is successful or not.
+     */
     @Test
     void testNonNull(){
         assertNotNull(aboutG);
--- src/test/java/UserGUITest.java
+++ src/test/java/UserGUITest.java
@@ -4,29 +4,53 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ *  @author Phuoc Le
+ *  Date 5/31/22
+ *
+ * This class is made to test out UserGUI class.
+ */
 class UserGUITest {
     private UserGUI userG;
+
+    /**
+     * Set up before we test all methods
+     */
     @BeforeEach
     void setUp() {
         userG = new UserGUI("Le");
     }
 
+    /**
+     * testNonNull is to check if the initiation of object is successful or not.
+     */
     @Test
     void testNonNull(){
         assertNotNull(userG);
     }
 
+    /**
+     * testUserGUI is to check if the path is saved correctly or not.
+     */
     @Test
     void testUserGUI(){
         assertEquals("User\\Le\\", userG.getPath());
     }
 
+
+    /**
+     * testNewUserGUI is to check if new user is added correctly.
+     */
     @Test
     void testNewUserGUI(){
         UserGUI newUser = new UserGUI ("JohnWick");
         assertNotNull(newUser);
         assertEquals("User\\JohnWick\\", newUser.getPath());
     }
+
+    /**
+     * testUserGUITile is to check if title is set correctly or not.
+     */
     @Test
     void testUserGUITile(){
         assertEquals("Le", userG.getTitle());
